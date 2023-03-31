9dd524aaf007a072077e6274ead7f056c859dd98
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2023-03-30 21:19:01 -0300

    Validação que certifica se o número é permito e e exibe a mensagem na tela

---
+++ app/validacao.js
@@ -0,0 +1,26 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+  const numero = +chute
+
+  if (chuteForInvalido(numero)) {
+    elementoChute.innerHTML += '<div>Valor inválido</div>'
+  }
+
+  if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+    elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+      <h2>Você acertou!!!</h2>
+      <h3>O número secreto era ${numeroSecreto}</h3>
+    `
+  }
+}
+
+function chuteForInvalido(numero) {
+  return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+  return numero > maiorValor || numero < menorValor
+}
